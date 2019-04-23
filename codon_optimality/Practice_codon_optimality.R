fecc521d04bada6104d465879445964cfafe7bbe
Erod Baybay <48958791+erodb@users.noreply.github.com>
2019-04-23 17:16:25 -0400

    Delete Practice_codon_optimality.R

--- codon_optimality/Practice_codon_optimality.R
+++
@@ -1,3 +0,0 @@
-library(tidyverse)   # install.packages("tidyverse")
-# ------------------------------------------------------------------------------------------
-data <- read.csv("https://tinyurl.com/codon-eric")
