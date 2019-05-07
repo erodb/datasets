aa4bd845d9d3fe1838207406bb81d6e9431d790d
Erod Baybay <48958791+erodb@users.noreply.github.com>
2019-05-07 14:52:51 -0400

    Update Education_Data.R

--- education/Education_Data.R
+++ education/Education_Data.R
@@ -1,4 +1,4 @@
-library(tidyverse)
+library(tidyverse) # install.packages("tidyverse")
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------
 A <- read.csv("https://raw.githubusercontent.com/erodb/datasets/master/education/Education_Data.csv", header = TRUE)
